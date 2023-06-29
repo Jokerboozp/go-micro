0633c945be2da8d665642acbfd8ffd60d8c8e36c
jokerboozp <2929590482@qq.com>
2023-06-29 13:43:35 +0800

    22

--- authentication-service/cmd/api/routes.go
+++ authentication-service/cmd/api/routes.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
-	"net/http"
 )
 
 func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
+	// specify who is allowed to connect
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://*", "https://*"},
-		AllowedMethods:   []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
